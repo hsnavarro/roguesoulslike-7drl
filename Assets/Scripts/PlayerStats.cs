f890adf0bbe469d9fe5cc73f9524a4ee70676da3
Henrique Navarro <ricksnavarro@gmail.com>
2021-03-07 16:33:26 -0400

    Add player stats (HP, Stamina, and others)

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float maxHP = 100f;
+    public float maxStamina = 100f;
+    public float normalSpeed = 5f;
+    public float runSpeed = 10f;
+
+    public float staminaRunDecreaseRate = 15f;
+    public float staminaRechargeRate = 20f;
+    public float staminaRechargeDelay = 5f;
+
+    public float HP;
+    public float Stamina;
+    public float speed;
+
+    private void Start()
+    {
+        HP = maxHP;
+        Stamina = maxStamina;
+        speed = normalSpeed;
+    }
+}
