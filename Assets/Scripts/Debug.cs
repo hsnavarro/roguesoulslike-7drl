7fb79240b17cd8cf004fb4dbc9560e6475dfd88f
Henrique Navarro <ricksnavarro@gmail.com>
2021-03-07 16:34:08 -0400

    Add debug mode, use checkbox to stop printing to the console

---
+++ Assets/Scripts/Debug.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug : MonoBehaviour
+{
+    [SerializeField]
+    public bool DebugOn = true;
+    
+    private static bool isDebugActive;
+
+
+    new static public void print(object message)
+    {
+        if (isDebugActive) MonoBehaviour.print(message);
+    }
+
+    private void Start()
+    {
+        isDebugActive = DebugOn;
+    }
+}
