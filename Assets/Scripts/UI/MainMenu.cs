5b771efb873c0ecf36547c50d4c2c495cedac51c
Luciano Barreira <luc.s.barreira@gmail.com>
2021-03-13 00:10:35 -0300

    Add main menu scene

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame(InputAction.CallbackContext callbackContext) {
+        SceneManager.LoadScene("Scenes/Naum");
+    }
+}
